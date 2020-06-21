0f1613fe13ac7ef946c63905293e1b37e8236e0d
Aryashree Mishra <aryamishra24@rediffmail.com>
2020-06-21 15:10:10 +0530

    Steup file for Heroku App

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -95,6 +96,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on Port 3000. ')
+app.listen(port, () => {
+    console.log('Server is up on Port '+ port)
 })
