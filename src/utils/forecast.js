f940490a0ca94adba59db330b834652bc53d20bc
Aryashree Mishra <aryamishra24@rediffmail.com>
2020-06-21 15:55:00 +0530

    Addd some new weather forecast features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location',undefined)
         }else{
-            callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature+'F and there is '+body.currently.precipProbability * 100 + '% chance of rain.'
+            callback(undefined, body.daily.data[0].summary+' It is currently '+body.currently.temperature +'.'+' The high tody will be '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow +'. There is '+body.currently.precipProbability * 100 + '% chance of rain. '+'Currently, the humidity is '+body.currently.humidity *100+' %.'
             )
         }
     })
